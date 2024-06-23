99a1e9e8e5dc5baa4fbead3f679f47ffa2ed40b9
tanmayswain11 <161924065+tanmayswain11@users.noreply.github.com>
2024-06-23 13:30:55 +0530

    Add files via upload

---
+++ Intermediate level/interactive button/script.js
@@ -0,0 +1,8 @@
+function hideshow() {
+    const mainDiv = document.getElementById('main');
+    if (mainDiv.style.display === 'none') {
+        mainDiv.style.display = 'block';
+    } else {
+        mainDiv.style.display = 'none';
+    }
+}
